a33e860aa93a5be5e316a67969bfd035eb06309f
AnnaGuillamet <guillametanna@gmail.com>
2022-02-22 18:36:30 +0100

    Added skeleton of main

---
+++ src/main.py
@@ -0,0 +1,15 @@
+''' Julian
+
+Usage:
+    Julian control
+    Julian --help
+Julian is a sowfware for xxxxxxx
+'''
+
+
+def main():
+    print("Hello from Julian")
+
+
+if __name__=="__main__":
+    main()
