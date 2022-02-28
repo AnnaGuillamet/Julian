366292b5269075c421084577073d7c32328581b4
AnnaGuillamet <guillametanna@gmail.com>
2022-02-28 16:50:42 +0100

    modificacio Decorador

--- src/main.py
+++ src/main.py
@@ -6,11 +6,15 @@ Usage:
 Julian is a sowfware for xxxxxxx
 '''
 
+from email.policy import default
 import enclosure as e  #e.blablabla. Sempre agafara el arxiu __init__.py
 from farm import Farm 
 
+import click
 
-def main():
+@click.command()  ##Decorador
+@click.option("-c","--configuration", default = "farm.cfg", type = click.Path("rb"), help="Configuration")
+def main(configuration):
     print("Hello from Julian")
     sensor = e.Sensor()
 
